5e3f8d49f15452444ac1587f62209fdef74eaa69
hartface <hartfacedev@gmail.com>
2026-06-08 13:46:19 +0300

    wip: added init skeleton

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+bl_info = {
+    "name": "Aiming Helper",
+    "author": "Hartface",
+    "description": "A visualizing tool, to help with aligning sights.",
+    "version": (1, 0, 0),
+    "blender": (5, 0, 0),
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == "__main__":
+    register()
